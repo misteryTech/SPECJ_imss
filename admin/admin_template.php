7064ce9df06344432d5f02d828096d426f202b18
Miste Ry <reymarkescalante12@gmail.com>
2024-06-27 16:49:06 +0800

    SERVICES & IMS
    GFI SCHOOL THESIS

---
+++ admin/admin_template.php
@@ -0,0 +1,24 @@
+<?php
+    include("admin_header.php");
+
+?>
+<body>
+
+     <!-- ======= Header ======= -->
+     <!-- ======= Sidebar ======= -->
+<?php
+    include("admin_topnav.php");
+    include("admin_sidenav.php");
+?>
+
+
+
+  <!-- Main-->
+
+
+  <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php
+    include("admin_footer.php");
+?>
---
+++ process/connection.php
@@ -0,0 +1,9 @@
+<?php
+        $localhost = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "specj_imss";
+
+
+        $connection = mysqli_connect("$localhost","$username","$password","$database");
+    ?>
